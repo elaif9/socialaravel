5766af8f62668b9197740d0c2d7aebe4e009f13d
Alif Nur Rachman <alifcombro907@gmail.com>
2017-05-19 22:01:27 +0700

    Edit Routes for sign up

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['web']], function(){
+    //
+    Route::post('/signup', [
+      'uses' => 'UserController@postSignUp',
+      'as' => 'signup'
+    ]);
+});
