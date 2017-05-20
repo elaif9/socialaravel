a0776656b5e21d320c206cdb8ef38ad57563efff
Alif Nur Rachman <alifcombro907@gmail.com>
2017-05-20 18:31:56 +0700

    Adding Dashboard Interface

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,44 @@
-<h1>The Dashboard</h1>
+@extends('layout.master')
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What happen today?</h3></header>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="10" placeholder="Text here"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>Timeline</h3></header>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet</p>
+                <div class="info">
+                    Posted By And Date Here
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet</p>
+                <div class="info">
+                    Posted By And Date Here
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
