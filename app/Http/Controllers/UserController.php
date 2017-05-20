652f7eb12c6f4cdad8d37d3a3ded7524b65c0b20
Alif Nur Rachman <alifcombro907@gmail.com>
2017-05-20 07:58:03 +0700

    Fixing the validation and edit the Kernel.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,10 +35,9 @@ class UserController extends Controller{
   }
 
   public function postSignIn(Request $request){
-    $this->validate($request, [
+    $this->validate($request,[
       'email' => 'required',
       'password' => 'required'
-
     ]);
 
     if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,34 +4,33 @@
    Welcome!
 @endsection
 
-
 @section('content')
   @if(count($errors) > 0)
-      <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-          <ul>
-            @foreach($errors->all() as $error)
-              <li>{{$error}}</li>
-            @endforeach
-          </ul>
-        </div>
-      </div>
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+      <ul>
+        @foreach($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+      </ul>
+    </div>
+  </div>
   @endif
 
   <div class="row">
     <div class="col-md-6">
       <h3>Sign In</h3>
        <form class="#" action="{{ route('signup')}}" method="post">
-         <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+         <div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
            <label for="email">Your Email</label>
            <input class="form-control" type="text" name="email" id="email" value="{{ Request::old('email') }}">
          </div>
-         <div class="form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
+         <div class="form-group {{ $errors->has('first_name') ? 'has-error' : ''}}">
            <label for="first_name">Your First Name</label>
            <input class="form-control" type="text" name="first_name" id="first_name" value="{{ Request::old('first_name') }}">
          </div>
-         <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
-           <label for="first_name">Your Passowrd</label>
+         <div class="form-group {{ $errors->has('password') ? 'has-error' : ''}}">
+           <label for="password">Your Password</label>
            <input class="form-control" type="password" name="password" id="password" value="{{ Request::old('password') }}">
          </div>
          <button type="submit" name="button" class="btn btn-primary">Submit</button>
@@ -41,12 +40,12 @@
     <div class="col-md-6">
       <h3>Sign In</h3>
        <form class="#" action="{{ route('signin') }}" method="post">
-         <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+         <div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
            <label for="email">Your Email</label>
            <input class="form-control" type="text" name="email" id="email" value="{{ Request::old('email') }}">
          </div>
-         <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
-           <label for="first_name">Your Passowrd</label>
+         <div class="form-group {{ $errors->has('password') ? 'has-error' : ''}}">
+           <label for="password">Your Password</label>
            <input class="form-control" type="password" name="password" id="password" value="{{ Request::old('password') }}">
          </div>
          <button type="submit" name="button" class="btn btn-primary">Submit</button>
